417a779f2f4897b01d227c46ae1cce274ea27989
folololo12 <91086365+folololo12@users.noreply.github.com>
2026-03-12 07:03:26 +0100

    Netflix EDA project

---
+++ Exploratory_data_analysis.py/Main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as mat
+
+df = pd.read_csv("netflix_titles.csv")
+
+#basid data
+print(df.head())
+print(df.info())
+print(df.describe())
+#trying out some stuff
+print(df.isnull().sum())
+print(df["type"].value_counts())
+print(df["country"].value_counts().head(10))
+#tworzenie wizualizacji
+sns.countplot(x="type", data=df)
+mat.title("Movies vs TV Shows")
+mat.xlabel("Type")
+mat.ylabel("Count")
+mat.show()
+top_countries = df["country"].value_counts().head(10)
+
+sns.barplot(x=top_countries.values, y=top_countries.index)
+mat.title("Top 10 Countries Producing Netflix Content")
+mat.xlabel("Number of Titles")
+mat.ylabel("Country")
+mat.show()
+
+top_directors = df["director"].value_counts().head(10)
+sns.barplot(x=top_directors.values, y =top_directors.index)
+mat.title("Top 10 Directors")
+mat.xlabel("how many movies")
+mat.ylabel("directors")
+mat.show()
